e6aecf5ddd638650ba271bb421ed746a5b4de22b
agustianmuhammadgilang-cloud <agustianmuhammadgilang@gmail.com>
2025-11-17 10:28:45 +0700

    ngatur routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-
+$routes->get('/login', 'Auth\Login::index');
+$routes->post('/login/process', 'Auth\Login::process');
+$routes->get('/logout', 'Auth\Login::logout');
 
